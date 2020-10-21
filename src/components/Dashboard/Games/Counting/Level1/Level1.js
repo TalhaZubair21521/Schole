6a6c39fcf788d2ce654915454e2cb5d21b279eaf
Muhammad Talha Zubair <talhazubair1598@gmail.com>
2020-10-21 15:52:30 +0500

    Counting Level 1 Completed

--- src/components/Dashboard/Games/Counting/Level1/Level1.js
+++ src/components/Dashboard/Games/Counting/Level1/Level1.js
@@ -12,21 +12,60 @@ import Ballon3 from "../../../../../assets/countingLevelsImages/b3.png";
 import Ballon4 from "../../../../../assets/countingLevelsImages/b4.png";
 import Ballon5 from "../../../../../assets/countingLevelsImages/b5.png";
 
-import { Link } from "react-router-dom";
 import BallonImage from "./DragImage/BallonImage";
 import CardImage from "./DropImage/CardImage";
+import { useHistory } from "react-router-dom";
 
 const Level1 = (props) => {
+    let history = useHistory();
     const ballons = [{ number: 10, src: Ballon1 }, { number: 8, src: Ballon2 }, { number: 6, src: Ballon3 }, { number: 4, src: Ballon4 }, { number: 2, src: Ballon5 }]
     const [ballonImages, setballonImages] = useState(ballons);
-    const cards = [{ id: 1, type: "empty", src: Ballon1 }, { id: 2, type: "empty", src: Ballon1 }, { id: 3, type: "empty", src: Ballon1 }, { id: 4, type: "empty", src: Ballon1 }, { id: 5, type: "empty", src: Ballon1 }]
+    const cards = [{ id: 1, type: "empty", src: "", number: 0 }, { id: 2, type: "empty", src: "", number: 0 }, { id: 3, type: "empty", src: "", number: 0 }, { id: 4, type: "empty", src: "", number: 0 }, { id: 5, type: "empty", src: "", number: 0 }]
     const [cardImages, setCardImages] = useState(cards);
 
-    const updateLocation = (id, cardId) => {
+    const evaluateResult = () => {
+        const completed = ((cardImages.filter(card => card.type === "empty")).length !== 0);
+        if (completed) {
+            alert("Complete the Game First");
+        } else {
+            const matcher = [2, 4, 6, 8, 10];
+            var ordered = true;
+            for (var i = 0; i < cardImages.length; i++) {
+                if (matcher[i] !== cardImages[i].number) {
+                    ordered = false;
+                    break;
+                }
+            }
+            if (ordered) {
+                alert("Right Order");
+                history.push("/dashboard/games/counting/level2");
+            } else {
+                alert("Wrong Order");
+                history.push("/dashboard/games/counting/level2");
+            }
+        }
+    }
 
+    const updateLocation = (id, cardId) => {
+        const newBallonsList = ballonImages.filter(ballon => ballon.number !== id);
+        const ballonDragged = ballonImages.filter(ballon => ballon.number === id);
+        const card = cardImages.filter(card => card.id === cardId);
+        if (card[0].type === "fill") {
+        } else {
+            setballonImages(newBallonsList);
+            card[0].src = ballonDragged[0].src;
+            card[0].type = "fill";
+            card[0].number = ballonDragged[0].number
+            const newCards = cardImages.filter((card) => {
+                if (card.id === cardId) {
+                    return card;
+                }
+                return true;
+            });
+            setCardImages(newCards);
+        }
     }
     return (
-
         <div>
             <Navbar seriesNumber={"1"} seriesLevel={"1"} seriesName={"Counting"} />
             <div style={mainDivHomepage}>
@@ -91,7 +130,7 @@ const Level1 = (props) => {
                         <div style={CenterContent}>
                             <div style={BackgroundImageSetter}>
                                 <div style={{ marginTop: "40px" }}>
-                                    <div className="row" style={{ textAlign: "center", margin: "auto", width: "60%", fontSize: "1.4rem", color: "red", fontWeight: "bold", marginBottom: "20px" }}>Drop Here</div>
+                                    <div className="row" style={{ textAlign: "center", margin: "auto", width: "60%", fontSize: "0.9rem", color: "#C84747", fontWeight: "bold", marginBottom: "20px" }}>Drop Here In Ascending Order</div>
                                     <div className="row">
                                         <div className="col-2" />
                                         <div className="col-4">
@@ -138,9 +177,7 @@ const Level1 = (props) => {
                                             }
                                         </div>
                                         <div className="col-4">
-                                            <Link to="/dashboard/games/counting/level2">
-                                                <img width="100%" style={{ marginTop: "25px" }} src={Submit} alt="box" />
-                                            </Link>
+                                            <img width="100%" style={{ marginTop: "25px" }} src={Submit} onClick={evaluateResult} alt="box" />
                                         </div>
                                         <div className="col-2" />
                                     </div>
